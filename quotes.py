c542d2ec72f819112d4b363f3261258a54c33cdf
Jonathan <deschuyteneerj@gmail.com>
2021-09-08 21:55:55 +0200

    correction

--- quotes.py
+++ quotes.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:root@localhost/quotes'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:root@localhost/quotes'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gzbxhmegnmjpql:420246b6464555cab2c2f11bffd29c74671daa6ab71326eb73ac6cecb9a03705@ec2-34-251-245-108.eu-west-1.compute.amazonaws.com:5432/d14ookjqn3t925'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
